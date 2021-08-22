9611ee53e913c420b297301528ab801fb6179553
catupperman <catupperman@gmail.com>
2021-08-22 16:06:53 -0400

    adding notes to sql docs

--- db/schema.sql
+++ db/schema.sql
@@ -2,19 +2,12 @@ DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 
 USE employee_db;
-
+--generates the department table in the data base
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30) NOT NULL
 );
-
--- CREATE TABLE course_names (
---   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
---   name VARCHAR(30) NOT NULL,
---   department INT,
---   ON DELETE SET NULL
--- );
-
+--generates the role table in the data base
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
@@ -23,7 +16,7 @@ CREATE TABLE role (
     REFERENCES department(id)
     ON DELETE SET NULL
 )
-
+--generates the employee table in the data base
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
@@ -33,5 +26,4 @@ CREATE TABLE employee (
     FOREIGN KEY (employee)
     REFERENCES manager(id)
     ON DELETE SET NULL
-
 )
